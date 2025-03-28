6adfe0c2b527c547872848a597d3d9a998add729
rudiHammad <rudihammad@gmail.com>
2025-03-28 01:41:47 +0100

    Add files via upload

---
+++ AppDelegate.cpp
@@ -0,0 +1,111 @@
+#include "AppDelegate.h"
+#include "_testScenes/inputScene.h"
+
+
+#define USE_AUDIO_ENGINE 1
+
+#if USE_AUDIO_ENGINE
+#    include "audio/AudioEngine.h"
+#endif
+
+using namespace ax;
+
+
+int resolutionScale = 2;
+static ax::Size designResolutionSize = ax::Size(640, 360);
+
+AppDelegate::AppDelegate() {}
+
+AppDelegate::~AppDelegate() {}
+
+// if you want a different context, modify the value of glContextAttrs
+// it will affect all platforms
+void AppDelegate::initGLContextAttrs()
+{
+    // set OpenGL context attributes: red,green,blue,alpha,depth,stencil,multisamplesCount
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8, 0};
+    // since axmol-2.2 vsync was enabled in engine by default
+    // glContextAttrs.vsync = false;
+
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
+bool AppDelegate::applicationDidFinishLaunching()
+{
+    // initialize director
+    auto director = Director::getInstance();
+    auto glView   = director->getGLView();
+    if (!glView)
+    {
+#if (AX_TARGET_PLATFORM == AX_PLATFORM_WIN32) || (AX_TARGET_PLATFORM == AX_PLATFORM_MAC) || \
+    (AX_TARGET_PLATFORM == AX_PLATFORM_LINUX)
+        glView = GLViewImpl::createWithRect(
+            "core", ax::Rect(0, 0, designResolutionSize.width * resolutionScale, designResolutionSize.height * resolutionScale));
+
+#else
+        glView = GLViewImpl::create("core");
+#endif
+        director->setGLView(glView);
+    }
+
+    // turn on display FPS
+    director->setStatsDisplay(true);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0f / 60);
+
+    // Set the design resolution
+    glView->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height,
+                                    ResolutionPolicy::SHOW_ALL);
+
+    ///////////////////////////////
+    // initialize steam api here //
+    ///////////////////////////////
+
+    // 1. initialize Steam API 
+    if (!SteamAPI_Init())
+    {
+        AXLOG(">>> Failed to initialize SteamAPI. Ensure Steam app is launched and that 'steam_appid.txt' is present in \n"
+        "the output directory. For instance axmolProject/build/bin/axmolProject/Debug (or /Release)");
+        return false;
+    }
+    else {  AXLOG(">>> SteamAPI initialized successfully");  }
+
+    // 2. Initialize Steam Input
+    if(SteamInput())
+    {
+        SteamInput()->Init(true);
+        AXLOG(">>> SteamInput initialized successfully");
+    }
+    else {  AXLOG(">>> Failed to initialize SteamInput");    }
+
+
+
+    auto scene = utils::createInstance<InputScene>();                         
+
+
+    // run
+    director->runWithScene(scene);
+
+    return true;
+}
+
+// This function will be called when the app is inactive. Note, when receiving a phone call it is invoked.
+void AppDelegate::applicationDidEnterBackground()
+{
+    Director::getInstance()->stopAnimation();
+
+#if USE_AUDIO_ENGINE
+    AudioEngine::pauseAll();
+#endif
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground()
+{
+    Director::getInstance()->startAnimation();
+
+#if USE_AUDIO_ENGINE
+    AudioEngine::resumeAll();
+#endif
+}
---
+++ inputScene.h
@@ -0,0 +1,118 @@
+/*
+ * Input test scene.
+ * 
+ * In this scene we learn of to implement input coming from a keyboard and a gamepad.
+ * 
+ */
+#pragma once
+#include "axmol.h"
+
+#include "steam/steam_api.h"
+#include "steam/isteaminput.h"
+
+
+USING_NS_AX;
+
+
+class InputScene: public ax::Scene
+{
+    enum class GameState
+    {
+        init = 0,
+        update,
+        pause,
+        end,
+        menu1,
+        menu2,
+    };
+    
+public:
+    bool init() override;
+    void update(float delta) override;
+
+    void handleSteamInput();
+    void testControllerIndices();
+
+    // Keyboard
+    void onKeyPressed(ax::EventKeyboard::KeyCode code, ax::Event* event);
+    void onKeyReleased(ax::EventKeyboard::KeyCode code, ax::Event* event);
+
+
+    InputHandle_t inputHandle;
+    InputActionSetHandle_t gameplaySet;
+    InputActionSetHandle_t currentGameplaySet;
+
+
+private:
+    entt::registry registry;
+};
+
+
+bool InputScene::init()
+{
+
+    if (!Scene::init())
+    {
+        return false;
+    }
+
+    auto visibleSize = _director->getVisibleSize();
+    auto origin      = _director->getVisibleOrigin();
+    auto safeArea    = _director->getSafeAreaRect();
+    auto safeOrigin  = safeArea.origin;
+
+            
+    // Check contected controllers
+    int numControllers = SteamInput()->GetConnectedControllers(&inputHandle);
+    AXLOG(">>> %i", numControllers);
+
+    
+    scheduleUpdate();
+
+    return true;
+}
+
+
+void InputScene::handleSteamInput()
+{
+       
+}
+
+
+void InputScene::testControllerIndices()
+{
+
+}
+
+
+void InputScene::update(float delta)
+{
+    if (SteamInput())
+    {
+        SteamInput()->RunFrame(); // Update Steam Input state
+    }
+
+
+    int numControllers = SteamInput()->GetConnectedControllers(&inputHandle);
+    //AXLOG(">>> %i controllers connected.", numControllers);
+
+
+    // Ensure we have the correct action set handle
+    gameplaySet = SteamInput()->GetActionSetHandle("ship_controls");
+    if (gameplaySet != 0) {
+        SteamInput()->ActivateActionSet(0, gameplaySet);
+    }
+
+    // Get the handle for the 'fire_lasers' button action (from the vdf)
+    InputDigitalActionHandle_t fireAction = SteamInput()->GetDigitalActionHandle("fire_lasers");
+    
+    if (fireAction != 0) {
+        // Get the action data (for a button action, not a trigger)
+        InputDigitalActionData_t actionData = SteamInput()->GetDigitalActionData(inputHandle, fireAction);
+        
+        // Check if the button is pressed (bState will be true if pressed)
+        if (actionData.bState) {
+            AXLOG(">>> Fire button pressed!");
+        }
+    }
+}
